5f145492c38bb3d97e078a75c96cf8263fe1dd63
karan <aayushrawa66t@gmail.com>
2026-02-18 01:54:09 +0530

    Initial commit: setup job portal fullstack project

---
+++ Backend/jobportal/jobportal/src/main/java/com/aayush/UserController.java
@@ -0,0 +1,25 @@
+package com.aayush;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
